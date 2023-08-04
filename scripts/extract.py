0e7242c87e3f12b117004d4857398adaf1b226f8
Jessica Severin <severin@anx247.gsc.riken.go.jp>
2023-08-04 09:13:22 +0900

    added argparse.ArgumentParser and args options. cleaned up code

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys, numpy, os, re, itertools, random
 import pysam
+import argparse
 
 sys.path.append("/anaconda/lib/python2.7/site-packages")
 #from intervaltree import Interval, IntervalTree
@@ -8,36 +9,56 @@ sys.path.append("/anaconda/lib/python2.7/site-packages")
 from datetime import datetime
 from multiprocessing import Process, Lock, Manager
 
-if len(sys.argv) < 6:
-    print("Usage: python gapanalysis.py inputbam outputbam glenlog minlen npro")
-    print("input sam can be either sorted based on coordinates or unsorted")
-    print("glenlog: default -1, same as scoreGenomicLengthLog2scale")
-    print("minlen: minimum length for segment to be in the database, default 15")
-    print("right now the TAGs are not accurate")
-    sys.exit()
+max_reads = 3000000
+max_aligns = 10000000
+mode = "maxreads"
 
-inputfile = sys.argv[1]
-outprefix = sys.argv[2]
-glenlog = float(sys.argv[3]) #default -1. Need to test the parameters. 
-minlen = int(sys.argv[4]) #min length for segment to be in the junction database
-npro = int(sys.argv[5]) #number of processors to use
-nonconreads = {} #dictionary to store all noncontinuous reads. need large memory
+
+parser = argparse.ArgumentParser(
+                    prog='extract.py',
+                    description='extract alignments from a QNAME (-n) sorted bam/sam based on either maxread or maxalignment counts. Always returns all aligments for each read')
+
+parser.add_argument('inputfile', help="sam/bam filename")           # positional argument
+parser.add_argument('outprefix', help="output prefix" )         # positional argument
+parser.add_argument('-r', '--maxreads', type=int,
+                    help="stop extraction at max read count <default 3000000 reads>")
+parser.add_argument('-a', '--maxaligns', type=int,
+                    help="stop extraction when alignment count exceeds max. always extracts all alignments for each read")
+parser.add_argument('-v', '--verbose', action='store_true')  # on/off flag
+
+args = parser.parse_args()
+#print(args)
+
+inputfile = args.inputfile
+outprefix = args.outprefix
+
+if(args.maxreads != None):
+    max_reads = args.maxreads
+    mode = "maxreads"
+    print("mode maxreads",max_reads)
+elif(args.maxaligns != None):
+    max_aligns = args.maxaligns
+    mode = "maxaligns"
+    print("mode alignments",max_aligns)
+
 
 starttime = datetime.now()
+inputbam = None
 if(inputfile.endswith("sam")):
     print(inputfile," is SAM")
-    inputbam = pysam.AlignmentFile(inputfile, "r", require_index=False)
+    try:
+        inputbam = pysam.AlignmentFile(inputfile, "r", require_index=False)
+    except OSError as e:
+        sys.exit()
 if(inputfile.endswith(".bam")):
     print(inputfile, " is BAM")
-    inputbam = pysam.AlignmentFile(inputfile, "rb", require_index=False)
-
-if(inputbam is None):
-    print("Usage: gaptypes3.py inputbam outprefix glenlog minlen npro")
-    print("   input: sam/bam file can be either sorted based on coordinates or unsorted. must use .sam or .bam input file")
-    print("   glenlog: default -1, same as scoreGenomicLengthLog2scale")
-    print("   minlen: minimum length for segment to be in the database, default 15")
-    print("   right now the TAGs are not accurate")
-
+    try:
+        inputbam = pysam.AlignmentFile(inputfile, "rb", require_index=False)
+    except OSError as e:
+        sys.exit()
+if(inputbam is None): 
+    print("\nERROR: input file incorrect\n")
+    parser.print_usage() #print_help
     sys.exit()
 
 
@@ -49,9 +70,6 @@ noncontinuous_count = 0;
 outputbam = pysam.AlignmentFile(outprefix+".bam", "wb", template=inputbam)
 logfile = open(outprefix + 'log.out', 'w')
 
-max_reads = 3000000
-max_aligns = 10000000
-
 
 ################################################################################
 
@@ -62,20 +80,12 @@ def logmsg(logstr):
     print(logstr, end='')
 
 
-
-
 ################################################################################
 #
-# main loop write out continuous alignments and group reads for analysis
+# main loop group alignments by read(QNAME) and write out until max cutoff
 #
 ################################################################################
 
-#might need to do 2 passes through the file
-# 1st pass to find the intvlslongall and connections hashes (should be small memory)
-# 2nd pass to process read QNAME alignment-groups
-
-
-#pass #1 performs check for continuous vs non-continuous and build long-connections
 inputcount=0
 readcount=0
 badcount=0
@@ -85,26 +95,11 @@ t_noncont_count=0;
 for align in inputbam:
     inputcount+=1 
     
-#    if not inputcount%1000000: #write output after every 1 million reads
-#        t_time = datetime.now()
-#        difftime = (t_time-starttime).seconds
-#        rate = ((inputcount/1000000) / difftime) * 60
-#        logmsg(timenow()+" Phase1-connections "+str(inputcount/1000000)+"mil alignments ["+
-#               str(readcount)+" reads] "+
-#               "("+str(difftime)+"sec, "+("%.3f" % rate)+" mega-align/min)\n")
-
     QNAME = align.query_name  #line[0]
     CIGAR = align.cigarstring  #line[5]
     FLAG = align.flag
     #SEQ   = align.query_sequence
     #QUAL  = align.qual
-
-    # this is the only logic for deciding if line goes into nonconreads
-    #if "SA:Z:" not in line.split()[-1] and 'N' not in line.split()[5]:
-    #    contalign.append(line); contcount+=1; continue
-    #if line.split()[0] in nonconreads: nonconreads[line.split()[0]].append(line); noncontinuous_count+=1
-    #else: nonconreads[line.split()[0]]=[line]; noncontinuous_count+=1
-
 
     if((not align.has_tag('SA')) and ('N' not in CIGAR)):
         continuous_count+=1; 
@@ -121,8 +116,10 @@ for align in inputbam:
         for t_align in current_aligns: 
             outputbam.write(t_align);            
         current_qname = QNAME  #reset to the next QNAME block
-        if(readcount >= max_reads): break
-        #if(inputcount >= max_aligns): break
+
+        if(mode=="maxreads" and (readcount >= max_reads)): break
+        if(mode=="maxaligns" and (inputcount >= max_aligns)): break
+
         del current_aligns
         
         if(readcount%100000==0): #write output after every 1 million reads
